dd0b18a472e6b1db6ea049f33affde027d178929
akgupta110398 <109887133+akgupta110398@users.noreply.github.com>
2024-03-06 17:58:39 +0530

    selenium app

---
+++ App.java
@@ -0,0 +1,13 @@
+package Automate_selenium.Automate_a_Web_Application_With_Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
